71c05879bb7ea52dd90ce49185acf0f20e0abbb0
juhyeongKim <zxcvbm804@gmail.com>
2024-11-07 17:11:42 +0900

    feat : 상품 클래스 추가

---
+++ src/main/java/model/Production.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Production {
+    private String name;
+    private int price;
+    private int amount;
+    private String promotion;
+
+    public Production(String name, int price, int amount, String promotion) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+        this.promotion = promotion;
+    }
+}
---
+++ src/test/java/model/ProductionTest.java
@@ -0,0 +1,18 @@
+package model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ProductionTest {
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(
+        strings = {"탄산2+1"}
+    )
+    void 아이템_생성_테스트(String promotion) {
+        Production production = new Production("콜라", 1000, 10, promotion);
+        Assertions.assertThat(production).isInstanceOf(Production.class);
+    }
+}
